e029bfba1aeedb19b6ee11ec259855e59d322d39
Thaddeus Ong <67548448+bladedmistral@users.noreply.github.com>
2022-09-11 02:40:58 +0800

    Fixed Hitboxes + Sword Angle + Fonts

--- ArthursRage/Assets/Scripts/SwordPosition.cs
+++ ArthursRage/Assets/Scripts/SwordPosition.cs
@@ -4,25 +4,16 @@ using UnityEngine;
 
 public class SwordPosition : MonoBehaviour
 {
-    public GameObject Arthur;
-    public float speed = 1.0f;
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         Vector3 mouse = Input.mousePosition;
         Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(new Vector3 (mouse.x, mouse.y,transform.position.z));
-        Vector3 targetDirection = mouseWorld - Arthur.transform.position;
-            // The step size is equal to speed times frame time.
-        float singleStep = speed * Time.deltaTime;
-        // Rotate the forward vector towards the target direction by one step
-        Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
-        // Calculate a rotation a step closer to the target and applies rotation to this object
-        transform.rotation = Quaternion.AngleAxis(-90 + Mathf.Atan2(newDirection.y, newDirection.x) * Mathf.Rad2Deg,Vector3.forward);
+        Vector3 targetDirection = mouseWorld - transform.position;
+        transform.rotation = Quaternion.AngleAxis(Mathf.Atan2(-targetDirection.x, targetDirection.y) * Mathf.Rad2Deg,Vector3.forward);
     }
 }
